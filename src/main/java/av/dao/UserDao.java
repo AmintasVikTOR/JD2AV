217667738b1a6c8d8793bfe3832f61ca8e58ddc8
AmintasVikTOR <amintas.vik@gmail.com>
2020-07-07 15:54:45 +0300

    JDBC Start

---
+++ src/main/java/av/dao/UserDao.java
@@ -0,0 +1,14 @@
+package av.dao;
+
+import av.domain.User;
+
+import java.util.List;
+
+public interface UserDao {
+    /*CRUD operations*/
+    /*Create = Insert*/
+    /*Read = select*/
+    /*Update*/
+    /*Delete*/
+    List<User> findAll();
+}
